79700b8dba32ff542c4fd1f58b7ac2e15157f07e
Gheorghe Lucian <115155585+ElTheScreeps@users.noreply.github.com>
2023-06-27 22:08:34 +0300

    Update FIFO.v

--- FIFO.v
+++ FIFO.v
@@ -1,46 +1,45 @@
-module FIFO (clk, rst_n, write_en, read_en, data_in, data_out, full, empty);
+module FIFO(clk, rst_n, write_en, read_en, data_in, data_out, empty, full);
 
 parameter data_width = 8;
-parameter depth = 8;
 
 input                       clk;
 input                       rst_n;
 input                       write_en;
 input                       read_en;
 input      [data_width-1:0] data_in;
-
 output reg [data_width-1:0] data_out;
-output                      full;
 output                      empty;
+output                      full;
 
-reg [$clog2(depth)-1:0] write;
-reg [$clog2(depth)-1:0] read;
-reg [$clog2(depth)-1:0] count;
-reg [data_width-1:0]    mem[0:depth-1];
-reg [data_width-1:0]    data;
-    
-always @(posedge clk or negedge rst_n) begin
-    if (!rst_n) begin
-        count <= 0;
-        read  <= 0;
-        write <= 0;
+reg [2:0] write;
+reg [2:0] read;
+reg [data_width-1:0] memory [data_width-1:0];
+
+assign full = ((write == 3'b111) & (read == 3'b000) ? 1 : 0 );
+assign empty = (write == read) ? 1 : 0;
+
+always @(posedge clk or negedge rst_n)
+begin
+    if (~rst_n) begin
+        memory[0] <= 0;
+        memory[1] <= 0; 
+        memory[2] <= 0; 
+        memory[3] <= 0;
+        memory[4] <= 0; 
+        memory[5] <= 0; 
+        memory[6] <= 0; 
+        memory[7] <= 0;
+        data_out  <= 0; 
+        write     <= 0; 
+        read      <= 0;
     end
-    else begin
-        if (write_en && !full) begin
-            mem[write] <= data_in;
-            write <= write + 1;
-            count <= count + 1;
-        end
-        if (read_en && !empty)begin
-            data_out <= mem[read];
-            read <= read + 1;
-            count <= count - 1;
-        end
+    else if (write_en & full == 0) begin
+        memory[write] <= data_in;
+        write <= write + 1;
+    end
+    else if (read_en & empty == 0) begin
+        data_out <= memory[read];
+        read <= read + 1;
     end
 end
-
-
-assign full = (count == 8'b00001000);
-assign empty = (count == 0);
-
 endmodule
